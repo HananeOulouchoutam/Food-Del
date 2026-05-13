f8e562c8f51950d8924669946a01b28c34e5feea
HananeOulouchoutam <hanane.oulouchoutam@eidia.ueuromed.org>
2026-05-13 10:24:32 +0100

    backend food : add , list , remove

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import dns from "node:dns";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoutes.js";
+
+// Google DNS
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+//app config
+const app = express();
+const port = 4000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//db connection
+connectDB();
+
+// api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`);
+});
